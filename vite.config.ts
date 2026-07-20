1ab7ea0803205f1cd172b54181bdae6a4771a143
Arnold Ceplauskas <arnoldceplauskas@gmail.com>
2026-07-20 17:08:38 -0600

    Fix base path to repository name

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Add this line to fix the 404 asset paths
+  base: "/ATSDialFactory/",
 });
